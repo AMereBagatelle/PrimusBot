a7daa09b2d862b850b1847a7fae18b224bf95e7b
AMereBagatelle <brennenputh@gmail.com>
2019-12-21 12:44:17 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        if message.author == client.user:
+            return
+
+        if message.content.startswith('.set-channel'):
+            writeToLineOfFile('constants.txt', 0, '{0.channel}\n'.format(message))
+            await message.channel.send('This channel is now the default for posting videos.')
+
+        if message.content.startswith('.startposting'):
+            writeToLineOfFile('constants.txt', 1, 'True')
+            await message.channel.send('I will now begin to post videos.')
+
+        if message.content.startswith('.stopposting'):
+            writeToLineOfFile('constants.txt', 1, 'False')
+            await message.channel.send('Stopping the video sending')
+
+
+        
+            
+def writeToLineOfFile(filename, line, content):
+    fp = open(filename, 'r')
+    lines = fp.readlines()
+    fp.close()
+    if len(lines) == 0:
+        lines = content
+        with open(filename, 'w') as fp:
+            fp.writelines(lines)
+    else:
+        lines[line] = content
+        fp = open(filename, 'w')
+        fp.writelines(lines)
+        fp.close()
+        
+def readLineOfFile(filename, line):
+    with open(filename) as fp:
+        return fp.readlines()[line]
+
+client = MyClient()
+client.run('NjU3NzEyMzY4Mjk3NDQzMzU4.Xf1mWg.35qKCUiW8Gu1lV7AdILwUsOXIyY')
